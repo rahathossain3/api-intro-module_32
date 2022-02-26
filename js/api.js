be34ebc22d74ed4cbce6bc9d636da95405014022
rahathossain3 <rahathossain210@gmail.com>
2022-02-27 03:33:03 +0600

    fetch system

---
+++ js/api.js
@@ -0,0 +1,11 @@
+function loadData() {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(data) = console.log(data)
+
+
+
+    // fetch('https://jsonplaceholder.typicode.com/todos/1')
+    //     .then(response => response.json())
+    //     .then(json => console.log(json))
+}
